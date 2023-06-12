cb0d273cb8c0857b71345a7cd34770e0ca23ce55
manguetownson <luna.artur1999@gmail.com>
2023-06-12 14:18:14 -0300

    Ajustes nas informações dos produtos

--- front-end/src/data/products.js
+++ front-end/src/data/products.js
@@ -1,38 +1,38 @@
 export const products = [
     {
         id: 1,
-        name: 'Teclado Mecanico RGB',
-        description: 'Teclado Mecanico con luces RGB switches cherry red',
+        name: 'Mouse Dell GAMER COM FIO ALIENWARE | AW320M',
+        description: 'Velocidade, conforto e poder de fogo nunca faltam neste mouse gamer com fio de tamanho reduzido. Ele conta com 19.000 DPI nativos e controle de velocidade de até 400 polegadas por segundo.',
         price: 1000
     },
     {
         id: 2,
         name: 'Samsung Smart TV 55',
-        description: 'Excelente tv LCD...',
+        description: 'Excelente smart tv com todas as plataformas de streaming mais populares',
         price: 3000
     },
     {
         id: 3,
-        name: 'Audifono Bluetooth Sony',
-        description: 'Audifono Bluetooth para smartphone',
+        name: 'Fone Bluetooth Sony',
+        description: 'Fone Bluetooth para smartphone',
         price: 770
     },
     {
         id: 4,
         name: 'Memoria Corsair 8GB DDR5',
-        description: 'Memoria RAM optimizada para Juegos',
+        description: 'Memoria RAM otimizada para Jogos',
         price: 3700
     },
     {
         id: 5,
         name: 'Asus Nvidia RTX',
-        description: 'Tarjeta Grafica para juegos en 4k',
+        description: 'Placa de vídeo para jogos em 4k',
         price: 5000
     },
     {
         id: 6,
         name: 'CPU Intel Core i5',
-        description: 'Optimizada para tareas multicore',
+        description: 'Optimizada para multitarefas',
         price: 4000
     },
 ]
