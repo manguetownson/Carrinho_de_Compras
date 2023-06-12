5d22c691eb879f0451fbc7fd96077c99f9b53d49
manguetownson <luna.artur1999@gmail.com>
2023-06-12 11:29:12 -0300

    método get para produtos

--- back-end/src/main/java/com/backend/carrinho_de_compras/controllers/ProductController.java
+++ back-end/src/main/java/com/backend/carrinho_de_compras/controllers/ProductController.java
@@ -8,8 +8,12 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class ProductController {
@@ -17,9 +21,14 @@ public class ProductController {
     @Autowired
     ProductRepository productRepository;
 
+    @PostMapping("/products")
     public ResponseEntity<ProductModel> saveProduct(@RequestBody @Valid ProductDTO productDTO){
         var productModel = new ProductModel();
         BeanUtils.copyProperties(productDTO, productModel);
         return ResponseEntity.status(HttpStatus.CREATED).body(productRepository.save(productModel));
+    }
+    @GetMapping("/products")
+    public ResponseEntity<List<ProductModel>> getAllProducts(){
+        return ResponseEntity.status(HttpStatus.OK).body(productRepository.findAll());
     }
 }
